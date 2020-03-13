7436a140b7426e4736cb937eb034f9e94d6caa47
George Makarenko <neoproductionco@gmail.com>
2020-03-13 11:30:26 +0200

    UpdateWorker now aware of the write result and retries if not successful

--- app/src/main/java/com/neoproduction/gasolinni/UpdateWorker.kt
+++ app/src/main/java/com/neoproduction/gasolinni/UpdateWorker.kt
@@ -1,23 +1,36 @@
 package com.neoproduction.gasolinni
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.neoproduction.gasolinni.data.FirebaseManager
 import com.neoproduction.gasolinni.data.Repository
+import java.util.concurrent.CountDownLatch
 
 class UpdateWorker(appContext: Context, workerParams: WorkerParameters) :
-    Worker(appContext, workerParams) {
+    Worker(appContext, workerParams), FirebaseManager.ResultListener {
+
+    private lateinit var countDownLatch: CountDownLatch
+    private lateinit var result: Result
 
     override fun doWork(): Result {
         val repository = Repository.getInstance(applicationContext)
         val stations = repository.getStations()
 
+        countDownLatch = CountDownLatch(1)
         val firebaseManager = FirebaseManager()
-        firebaseManager.signInToFirebaseAndUpdate(stations)
+        firebaseManager.signInToFirebaseAndUpdate(stations, this)
+        countDownLatch.await()
 
-        applicationContext.setNeedSync(false)
+        Log.d("W0RK8R", "Worker finished")
+        return result
+    }
 
-        return Result.success() // or Result.retry() if couldn't do it
+    override fun onResult(result: Boolean) {
+        this.result = if (result) Result.success() else Result.retry()
+        applicationContext.setNeedSync(!result)
+        Log.d("W0RK8R", "OnResult: $result")
+        countDownLatch.countDown()
     }
 }
--- app/src/main/java/com/neoproduction/gasolinni/data/FirebaseManager.kt
+++ app/src/main/java/com/neoproduction/gasolinni/data/FirebaseManager.kt
@@ -9,12 +9,15 @@ import com.google.firebase.ktx.Firebase
 class FirebaseManager {
     private val firebaseAuth = FirebaseAuth.getInstance()
 
-    fun signInToFirebaseAndUpdate(stations: List<Station>) {
+    fun signInToFirebaseAndUpdate(
+        stations: List<Station>,
+        resultListener: ResultListener
+    ) {
         // Check if already signed in
         val user = firebaseAuth.currentUser
         if (user != null) {
             Log.d("TEST", "already signed in")
-            updateFirebaseDB(user, stations)
+            updateFirebaseDB(user, stations, resultListener)
         } else {
             Log.d("TEST", "was not signed in. signing in...")
             firebaseAuth.signInAnonymously().addOnCompleteListener { task ->
@@ -22,16 +25,21 @@ class FirebaseManager {
                     Log.d("TEST", "signed in successfully")
                     val userAfterSignIn = firebaseAuth.currentUser ?: return@addOnCompleteListener
                     Log.d("TEST", "got user uid")
-                    updateFirebaseDB(userAfterSignIn, stations)
+                    updateFirebaseDB(userAfterSignIn, stations, resultListener)
                     return@addOnCompleteListener
                 }
 
                 Log.d("TEST", "sign in failed")
+                resultListener.onResult(false)
             }
         }
     }
 
-    private fun updateFirebaseDB(user: FirebaseUser, stations: List<Station>) {
+    private fun updateFirebaseDB(
+        user: FirebaseUser,
+        stations: List<Station>,
+        resultListener: ResultListener
+    ) {
         val db = Firebase.database.reference
         Log.d("TEST", "setting value...")
         db.child("users").child(user.uid).child("stations").setValue(stations)
@@ -40,6 +48,16 @@ class FirebaseManager {
                     Log.d("TEST", "written data to the DB")
                 else
                     Log.d("TEST", "Error while writing to the DB")
+
+                resultListener.onResult(it.isSuccessful)
             }
+            .addOnCanceledListener {
+                Log.d("W0RK8R", "Write to DB cancelled")
+                resultListener.onResult(false)
+            }
+    }
+
+    interface ResultListener {
+        fun onResult(result: Boolean)
     }
 }
